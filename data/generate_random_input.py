847ba1fd106f7c6bacd93cde3404a76333efeb0a
liamdj <liamdj@princeton.edu>
2021-07-28 14:10:21 -0400

    Generate correct number of students

--- data/generate_random_input.py
+++ data/generate_random_input.py
@@ -6,12 +6,12 @@ import sys
 import numpy as np
 from collections import OrderedDict
 
-STUDENTS = 73
-
 if __name__ == "__main__":
    
     courses = pd.read_csv(sys.argv[1], index_col="Course", dtype = {"Course": str, "TA Slots": int}, usecols=["Course", "TA Slots"])
     courses["TA Slots"].fillna(1, inplace=True)
+
+    slots = courses["TA Slots"].sum()
 
     course_slots = np.array([course for course, slots in courses.itertuples() for _ in range(slots)]).astype(str)
 
@@ -20,7 +20,7 @@ if __name__ == "__main__":
         info_writer.writerow(["Student", "Net ID", "Previous Courses", "Advisor's Course", "Assign Weight"])
         pref_writer = csv.writer(pref_file)
         pref_writer.writerow(["Username", "Name", "Excellent Matches", "Favorite Course", "Good Matches", "Okay Matches", "Poor Matches", "Unqualified Matches"])
-        for i in range(STUDENTS):
+        for i in range(slots):
             nums = [np.random.randint(2, 5), np.random.randint(2, 9), np.random.randint(0, 9), np.random.randint(0, 9)]
             np.random.shuffle(course_slots)
             ordering = list(OrderedDict.fromkeys(course_slots))
@@ -35,7 +35,7 @@ if __name__ == "__main__":
             info_writer.writerow(["Student #{}".format(i), "student_{}".format(i), previous, advisors, ''])
             pref_writer.writerow(["student_{}@princeton.edu".format(i), "Student #{}".format(i), excellent, favorite, good, okay, poor, unqualified])
 
-    students = ["Student #{}".format(i) for i in range(STUDENTS)]
+    students = ["Student #{}".format(i) for i in range(slots)]
     with open("prof_pref.csv", "w", newline='') as file:
         writer = csv.writer(file)
         writer.writerow(["Course","Excellent Matches","Good Matches","Okay Matches","Poor Matches","Unqualified Matches"])
